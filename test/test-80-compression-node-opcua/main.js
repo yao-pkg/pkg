42864aebedc9df0fa2116a611b12f2f92319ff01
Daniel Lando <daniel.sorridi@gmail.com>
2024-02-16 15:01:01 +0100

    fix: use npx

--- test/test-80-compression-node-opcua/main.js
+++ test/test-80-compression-node-opcua/main.js
@@ -95,8 +95,12 @@ function pkgCompress(compressMode, output) {
 
 function esbuildBuild(entryPoint) {
   const log = utils.spawn.sync(
-    path.join(__dirname, 'node_modules/.bin/esbuild'),
+    path.join(
+      path.dirname(process.argv[0]),
+      'npx' + (process.platform === 'win32' ? '.cmd' : ''),
+    ),
     [
+      'esbuild',
       entryPoint,
       '--bundle',
       '--outfile=' + path.join(buildDir, pkgJson.main),
