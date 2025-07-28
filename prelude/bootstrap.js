9b97fa6b2a7ce92528a0ab76382ee604ee3e59cb
百川 <baizhuantao@bihu.mobi>
2025-07-28 19:58:57 +0800

    fix: use cache directory in users home instead of system-wide tmp dir

--- prelude/bootstrap.js
+++ prelude/bootstrap.js
@@ -711,7 +711,7 @@ function payloadFileSync(pointer) {
   });
   function deflateSync(snapshotFilename) {
     if (!tmpFolder) {
-      tmpFolder = fs.mkdtempSync(path.join(os.tmpdir(), 'pkg-'));
+      tmpFolder = fs.mkdtempSync(path.join(homedir(), '.cache/pkg', 'tmp-'));
     }
     const content = fs.readFileSync(snapshotFilename, { encoding: 'binary' });
     // content is already unzipped !
