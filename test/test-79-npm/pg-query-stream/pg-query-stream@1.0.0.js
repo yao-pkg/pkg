51a23a07a20697144120d2aa258017e4eb2fb242
robertsLando <daniel.sorridi@gmail.com>
2026-02-12 11:33:52 +0100

    style: format QueryStream instantiation for improved readability

--- test/test-79-npm/pg-query-stream/pg-query-stream@1.0.0.js
+++ test/test-79-npm/pg-query-stream/pg-query-stream@1.0.0.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var QueryStream = require('pg-query-stream');
-var query = new QueryStream('SELECT * FROM generate_series(0, $1) num', [
-  1000000,
-]);
+var query = new QueryStream(
+  'SELECT * FROM generate_series(0, $1) num',
+  [1000000],
+);
 if (query.state || (query.cursor && query.cursor.state)) {
   console.log('ok');
 }
